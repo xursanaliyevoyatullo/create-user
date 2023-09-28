caecf995269474443f288052976a10d49ca87a56
oyatulloxursanaliyev <xursanaliyevoyatullo5@gmail.com>
2023-09-28 21:55:00 +0500

    link-chanel

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <div className='footer'>
       <div className="footer-container container">
-        <p>All Right Reserved <a href="https://www.youtube.com/@akhrorweb">Xursanaliyev Oyatullo</a>
+        <p>All Right Reserved <a href="https://t.me/xursanaliyev_blog">Xursanaliyev Oyatullo</a>
         </p>
       </div>
     </div>
