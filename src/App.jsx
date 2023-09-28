f196cd9d0a323e3a1d483d0f805c2c4ff5da7adf
oyatulloxursanaliyev <xursanaliyevoyatullo5@gmail.com>
2023-09-28 21:17:48 +0500

    create-user

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+// style
+import './App.css'
+import { useState } from 'react'
+// components
+import Navbar from './components/navbar/Navbar'
+import Footer from './components/footer/Footer'
+import UserList from './components/userlist/UserList'
+import NewUserForm from './components/newuser/NewUserForm'
+
+function App() {
+  const [showModal, setShowModal] = useState(false)
+  const [users, setUsers] = useState([])
+
+  //delete users
+  const deleteUser = (id) =>{
+    setUsers((prev) => {
+      return prev.filter((user) => {
+        return user.id !== id 
+      })
+    })
+  }
+
+  //closeModal
+  const closeModal = (e) => {
+    if (e.target.className === 'overlay') setShowModal(false)
+    if (e.key === 'Escape') setShowModal(false)
+  }
+
+  //add User
+  const addUser = (user) => {
+    setUsers((prev) => {
+      return [...prev, user]
+    })
+    setShowModal(false)
+  }
+
+  return (
+    <div onClick={closeModal} onKeyDown={closeModal} className='App'>
+      <Navbar usersLength={users.length} />
+      <main>
+        <div className='no-users'>
+          {users.length === 0 && <h2>No Users</h2>}
+        </div>
+        <UserList users={users} deleteUser={deleteUser}/>
+      </main>
+      {showModal && <NewUserForm addUser={addUser}/>}
+      <button onClick={() => setShowModal(true)} className='create-user'>Create User</button>
+      <Footer />
+    </div>
+  )
+}
+
+
+export default App
+
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,15 @@
+// style
+import './Footer.css'
+
+function Footer() {
+  return (
+    <div className='footer'>
+      <div className="footer-container container">
+        <p>All Right Reserved <a href="https://www.youtube.com/@akhrorweb">Xursanaliyev Oyatullo</a>
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
